82eed53817055c22d39e1bc3edf9fde15b7491e1
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2022-03-14 13:27:40 -0600

    Update bootstrap.sh

--- Deploying_an_EC2_Instance_Demo/bootstrap.sh
+++ Deploying_an_EC2_Instance_Demo/bootstrap.sh
@@ -1,6 +1,18 @@
+
 #!/bin/bash  
 yum update -y
 yum install httpd -y
 echo "<html><body><h1>Hello Cloud Gurus</h1></body></html>" >/var/www/html/index.html
 systemctl start httpd
 systemctl enable httpd
+
+
+#ubuntu 
+
+#!/bin/bash  
+sudo -s
+sudo apt update -y
+sudo apt-get install apache2 -y
+sudo echo "<html><body><h1>Hello Cloud Gurus</h1></body></html>" >/var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
